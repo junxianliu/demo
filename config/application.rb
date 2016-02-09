e7e4da20b41c77a447af93c6717a00aefda7e54d
junxianliu <junxian.liu@thomson.co.uk>
2016-02-09 14:18:24 +0000

    web console error

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Demo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.web_console.whitelisted_ips = "10.33.110.104"
+    
+    config.web_console.whiny_requests = false
+    
   end
 end
